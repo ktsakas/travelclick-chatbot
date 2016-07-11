661cf66d0e30523d420a61afdedee6f1bee637ba
Konstantinos Tsakas <ktsakas@TRAVELCLICK.NET>
2016-07-11 13:35:24 -0400

    fixed scrollbar clearfix issue

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,82 @@
 var app = angular.module("chatbot", [/*'jsonFormatter'*/]);
+var sessionId = Math.floor(Math.random() * 1000000) + 1;
 
-app.controller("chatCtrl", function ($scope/*, $element, $http*/, $timeout) {
+app.controller("chatCtrl", function ($scope/*, $element*/, $http, $timeout) {
 	// Array of all answers (bot and user)
 	$scope.answers = [];
 	// Whether or not the input is disabled
 	$scope.disabled = false;
 
-	$scope.addMessage = function () {
-		console.log("add message!");
+	// Scrolls to the bottom of the chat
+	function scrollToBottom () {
+		$timeout(function () {
+			console.log("scrolling");
+			var scroller = document.body;
+			scroller.scrollTop = scroller.scrollHeight;
+		});
+	}
+
+	function handleAnswers (res) {
+		var answers = res.data.answers;
+
+		/*$scope.messages[$scope.messages.length - 1].analysis = 
+			res.data.analysis;*/
+
+		console.log("answers: ", answers);
+		answers.forEach(function (answer) {
+			answer.owner = "bot";
+			$scope.answers.push(answer);
+		})
+		/*for (var i= 0; i < answers.length; i++) {
+			answers[i].owner = "bot";
+
+			$scope.messages.push(answers[i]);
+		}*/
+
+		$scope.disabled = false;
+		scrollToBottom();
+	}
+
+	function removeTemporary () {
+		var tmps = document.getElementsByClassName("temporary");
+
+		for (var i= 0; i < tmps.length; i++) {
+			tmps[i].style.display = 'none';
+		}
+		// temporary
+	}
+
+	$scope.submitMessage = function () {
+		$scope.addMessage($scope.newMessage);
+	}
+
+	$scope.addMessage = function (newMsg) {
+		var equiv = $scope.equiv;
+
 		$scope.answers.push({
 			type: "msg",
 			owner: "user",
-			text: $scope.newMessage,
+			text: newMsg,
 		});
+
+		// Disable input and scroll to bottom
+		$scope.disabled = true;
+		scrollToBottom();
+		removeTemporary();
+
+		// Reset input values
+		$scope.newMessage = "";
+		$scope.equiv = null;
+
+		// Handle server reponse
+		$http.get("/chat/" + sessionId, {
+			params: { message: newMsg/*, knownEntities: this.knownEntities*/ }
+		}).then(handleAnswers);
+
 	};
 
 	// Test message
-	$scope.answers.push({
+	/*$scope.answers.push({
 		type: "msg",
 		owner: "user",
 		text: "Test user message",
@@ -53,7 +113,7 @@ app.controller("chatCtrl", function ($scope/*, $element, $http*/, $timeout) {
 			type: "map_directions",
 			owner: "bot",
 		});
-	}, 1000);
+	}, 1000);*/
 });
 
 // Display html instead of plain text
